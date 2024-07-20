d23174834f6e2d6acb9e4d954e91a66d729c2baf
cavidan-1907 <cavidanvelizade9@gmail.com>
2024-07-20 17:11:04 +0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { Oval } from 'react-loader-spinner';
+
 import Home from './pages/Home';
 import Login from './components/LOG/login';
 import Notes from './components/note/Notes';
 import Register from './components/LOG/register';
 
 function App() {
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-  
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 0);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-800">
-        <Oval color="#00BFFF" height={80} width={80} />
-      </div>
-    );
-  }
 
   return (
     <Router>
